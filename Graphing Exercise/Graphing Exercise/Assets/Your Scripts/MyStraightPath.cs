4e55cfdd6431270e8e53adf33cbdf236fd9fda9e
RileyDriscoll <RileyDriscoll97@gmail.com>
2018-09-10 23:39:24 -0700

    Started the project, line scene done

---
+++ Graphing Exercise/Graphing Exercise/Assets/Your Scripts/MyStraightPath.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyStraightPath : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector3(transform.position.x + 1 * Time.deltaTime, 0);
+	}
+}
